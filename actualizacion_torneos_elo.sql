7d7c9999358b1bd45278aa18b0c58d525a4dfcc7
evilloris <128542745+evilloris@users.noreply.github.com>
2026-07-16 18:00:54 -0400

    Delete actualizacion_torneos_elo.sql

--- actualizacion_torneos_elo.sql
+++
@@ -1,5 +0,0 @@
--- Ejecuta esto una sola vez si ya creaste las tablas con el SQL anterior.
-alter table public.matches
-add column if not exists elo_processed boolean not null default false;
-
-notify pgrst, 'reload schema';
